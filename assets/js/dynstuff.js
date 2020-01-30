d36c1f94b23d56e6054b5f775d137379cf499bef
Håkon Olsen <h.olsen@sportradar.com>
2020-01-30 13:55:21 +0100

    added login page

--- assets/js/dynstuff.js
+++ assets/js/dynstuff.js
@@ -1,10 +1,24 @@
-window.addEventListener("load", function () {
+var authcheck = function () {
     var user = netlifyIdentity.currentUser();
     var username = '';
-    if (user == 'undefined') {
-        username = 'Anonymous Donkey';
+    if (user == undefined) {
+        username = 'anonymous@donkey.business';
+        // check if page is allowed as anonymous
+        if (!window.location.pathname.includes('loginrequired')) {
+            window.location = "/loginrequired.html";
+        }
     } else {
         username = user.email;
     }
     document.getElementById("usrtag").innerHTML = username;
-})
+}
+
+window.addEventListener("load", authcheck());
+
+if (window.location.pathname.includes('loginrequired')) {
+    document.getElementById("loginbtn").addEventListener("click", function () {
+        netlifyIdentity.open();
+    })
+}
+
+netlifyIdentity.on("login", authcheck())
