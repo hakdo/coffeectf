52b9dd9740ae0bbc6dd4c9f01cad99e78fbf8fe9
Håkon Olsen <h.olsen@sportradar.com>
2020-01-30 15:27:15 +0100

    preliminary hack to avoid redirects

--- assets/js/dynstuff.js
+++ assets/js/dynstuff.js
@@ -1,16 +1,16 @@
 var allowed_nonauth = ['/', '/loginrequired', '/loginrequired.html'];
 
 var authcheck = function () {
-    var user = netlifyIdentity.currentUser();
     var username = '';
-    if (!user) {
+    var usertoken = localStorage.getItem("gotrue.user");
+    // need to check this at the next crossroads...
+    if (!usertoken) {
         username = 'anonymous@donkey.business';
         // check if page is allowed as anonymous
         if (!allowed_nonauth.includes(window.location.pathname)) {
+            console.log(netlifyIdentity.currentUser());
             window.location = "/loginrequired.html";
         }
-    } else {
-        username = user.email;
     }
     document.getElementById("usrtag").innerHTML = username;
 }
@@ -22,12 +22,13 @@ if (window.location.pathname.includes('loginrequired')) {
     })
 }
 
-window.addEventListener("load", authcheck());
-netlifyIdentity.on("login", function () {
-    document.getElementById("usrtag").innerHTML = netlifyIdentity.currentUser().email;
+netlifyIdentity.on("login", user => {
+    document.getElementById("usrtag").innerHTML = user.email;
 })
 
-netlifyIdentity.on("logout", function () {
+netlifyIdentity.on("logout", () => {
     document.getElementById("usrtag").innerHTML = "anonymous@donkey.business";
     window.location = "/";
 })
+
+document.body.addEventListener("load", authcheck());
