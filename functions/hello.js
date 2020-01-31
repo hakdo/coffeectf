6bc4b2ff9ae6fd3aa36e84efc689adfcb3ff2fec
Håkon Olsen <hakon@cybehave.com>
2020-01-31 10:36:44 +0100

    adding persistence to team creation....

--- functions/hello.js
+++ functions/hello.js
@@ -3,6 +3,8 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+var mongostr = 'mongodb://' + process.env.mongostring;
+const mongoose = require('mongoose');
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -14,15 +16,60 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+mongoose.connect(mongostr, {useNewUrlParser: true})
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error to db: '));
+db.once('open', function () {
+// db code goes here.
+  var memberSchema = new mongoose.Schema({
+    uid: String,
+    email: String,
+    avatar: String
+  });
+
+  var teamSchema = new mongoose.Schema({
+    name: String,
+    owner: String,
+    members: [memberSchema], // json with necessary user information to create a list and use for assignment
+    secret: String
+  });
+
+  var HackerTeam = mongoose.model('HackerTeam', teamSchema);
+
+  router.post('/teams/join', (req, res) => {
+    // get the type of join request
+    var jointype = req.body.jointype;
+    var contentdata = req.body.team;
+    if (jointype === 'craete') {
+      // create a new team
+      var myteam = {
+        name: contentdata,
+        owner: "test-test",
+        secret: "generated-funny-secret"
+      }
+      var newteam = new HackerTeam(myteam);
+      newteam.save(function (err, data) {
+        if (err) {
+          res.redirect('/')
+        } else {
+          // ok, se we crated a new team, let's just dump it to the browser first...
+          res.json(data)
+        }
+      })
+    } else {
+      // join a team
+      res.redirect('/?jointype=' + jointype + '%error=not-implemented');
+    }
+  })
+
+})
+
+
+
 router.get('/teams', (req, res) => {
   res.send('You will get teams here')
 })
 
-router.post('/teams/join', (req, res) => {
-  // get the type of join request
-  var jointype = req.body.jointype;
-  res.redirect('/?jointype=' + jointype)
-})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
