1faa2165356194ecf9927d2289986ff7a4343d15
Håkon Olsen <hakon@cybehave.com>
2020-01-31 14:45:16 +0100

    netlify forms...

--- functions/jointeam.js
+++ functions/jointeam.js
@@ -4,20 +4,43 @@ let conn = null;
 
 const uri = 'mongodb://' + process.env.mongostring;
 
+var kakeparser = function (cookie) {
+    var kakeboks = {};
+    var smuler = [];
+    try {
+        var kakemiks = cookie.split(/[;,]+/);
+        for (kakebit of kakemiks) {
+            smuler = kakebit.split("=");
+            kakeboks[smuler[0]] = smuler[1];
+        }
+    } catch (error) {
+        console.log("something bad happened. ", error)
+    }
+    return kakeboks
+}
+
+var getdatafromjwt = function (jwt) {
+    var payload = jwt.split('.')[1];
+    var decoded = Buffer.from(payload, 'base64').toString('utf8');
+    return JSON.parse(decoded);
+}
+
+var bodyparser = function (body) {
+    var payload = decodeURI(body)
+    var bodyparams = payload.split('&');
+    var cleanload = {}; var smuler = [];
+    for (param of bodyparams) {
+        smuler = param.split("=");
+        cleanload[smumer[0]] = smuler[1];
+    }
+    // remove url encoding...
+}
+
 exports.handler = async function(event, context, callback) {
-  // Make sure to add this so you can re-use `conn` between function calls.
-  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
   context.callbackWaitsForEmptyEventLoop = false;
 
-  // Because `conn` is in the global scope, Lambda may retain it between
-  // function calls thanks to `callbackWaitsForEmptyEventLoop`.
-  // This means your Lambda function doesn't have to go through the
-  // potentially expensive process of connecting to MongoDB every time.
   if (conn == null) {
     conn = await mongoose.createConnection(uri, {
-      // Buffering means mongoose will queue up operations if it gets
-      // disconnected from MongoDB and send them when it reconnects.
-      // With serverless, better to fail fast if not connected.
       bufferCommands: false, // Disable mongoose buffering
       bufferMaxEntries: 0 // and MongoDB driver buffering
     });
@@ -25,7 +48,10 @@ exports.handler = async function(event, context, callback) {
   }
 
   const M = conn.model('HackerTeams');
-
+  if (event.httpMethod == 'POST') {
+    // check if the name jointype parameter is set to create
+    var jointype = event.body
+  }
   const doc = await M.findOne();
   
   callback(null, {
