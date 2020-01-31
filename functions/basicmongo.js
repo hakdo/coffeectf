908877854707d5f550564e9e1b4210b6b8c7d005
Håkon Olsen <hakon@cybehave.com>
2020-01-31 12:07:56 +0100

    adding fun wo express

---
+++ functions/basicmongo.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+let conn = null;
+
+const uri = 'mongodb://' + process.env.mongostring;
+
+exports.handler = async function(event, context) {
+  // Make sure to add this so you can re-use `conn` between function calls.
+  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  // Because `conn` is in the global scope, Lambda may retain it between
+  // function calls thanks to `callbackWaitsForEmptyEventLoop`.
+  // This means your Lambda function doesn't have to go through the
+  // potentially expensive process of connecting to MongoDB every time.
+  if (conn == null) {
+    conn = await mongoose.createConnection(uri, {
+      // Buffering means mongoose will queue up operations if it gets
+      // disconnected from MongoDB and send them when it reconnects.
+      // With serverless, better to fail fast if not connected.
+      bufferCommands: false, // Disable mongoose buffering
+      bufferMaxEntries: 0 // and MongoDB driver buffering
+    });
+    conn.model('HackerTeams', new mongoose.Schema({name: String, owner: String, secret: String, members: [] }));
+  }
+
+  const M = conn.model('HackerTeams');
+
+  const doc = await M.findOne();
+  console.log(doc);
+
+  return doc;
+};
