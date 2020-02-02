53e61fb47d60130244a34dcce2fa5d9adf9ee2c3
Håkon Olsen <hakon@cybehave.com>
2020-02-02 11:04:45 +0100

    trying to figure out what fails...

--- assets/js/dynstuff.js
+++ assets/js/dynstuff.js
@@ -52,7 +52,8 @@ if (window.location.pathname.includes('jointeam')) {
     // get all the available teams...
     fetch('/.netlify/functions/basicmongo')
         .then((response) => {
-            teamlist(JSON.parse(response.json()))
+            console.log(response.json())
+            teamlist(response.json())
         })
         .catch((err) => {
             console.log(err);
