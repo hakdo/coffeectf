fa96965057322fbbf3d7bcc4c081f2ddbe31fdd4
Håkon Olsen <hakon@cybehave.com>
2020-02-05 22:44:36 +0100

    adding query params

--- functions/basicmongo.js
+++ functions/basicmongo.js
@@ -52,12 +52,12 @@ exports.handler = async function(event, context, callback) {
       var mininfo = getdatafromjwt(kaker['nf_jwt'])
   }
 
-  const { identity, user } = context.clientContext;
   console.log("Id: ", identity);
   console.log("User: ", user);
 
   const M = conn.model('HackerTeams');
-
+  var myparams = event.queryStringParameters;
+  console.log(myparams);
   const doc = await M.find({});
   
   callback(null, {
