69587b50dabe7e41450ec9800c0f85d0ab79e51a
Håkon Olsen <h.olsen@sportradar.com>
2020-02-05 10:17:26 +0100

    debug log

--- functions/createteam.js
+++ functions/createteam.js
@@ -51,6 +51,7 @@ exports.handler = async function(event, context, callback) {
   if (event.httpMethod == 'POST') {
     // Check if user is logged in, create new team if name is unique. Check in code instead of enforcing name as index. 
     const doc = await M.findOne({"name": "bingo"});
+    console.log(doc);
     callback(null, {statusCode: 200, body: JSON.stringify(doc)});
   }
 };
